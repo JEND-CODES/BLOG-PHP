b5ddddf20e1b6c90b01102c36c2d59f410fcb3a4
JEND-CODES <jeaneudes.nd@gmail.com>
2021-01-29 00:22:17 +0100

    Content touching up

---
+++ content/theme/js/pagination.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    // jQuery Plugin: http://flaviusmatis.github.io/simplePagination.js/
+    // Pagination des commentaires (plugin Jquery simplePagination.js)
+    var items = $(".list-wrapper");
+    var numItems = items.length;
+    var perPage = 2;
+
+    items.slice(perPage).hide();
+
+    $('#pagination-container').pagination({
+        items: numItems,
+        itemsOnPage: perPage,
+        prevText: "&laquo;",
+        nextText: "&raquo;",
+        onPageClick: function (pageNumber) {
+            var showFrom = perPage * (pageNumber - 1);
+            var showTo = showFrom + perPage;
+            items.hide().slice(showFrom, showTo).show();
+        }
+    });
+    
+
+});
--- content/theme/js/script.js
+++ content/theme/js/script.js
@@ -19,27 +19,5 @@ $(document).ready(function () {
         }, 400);
         return false;
     });
-
-    // jQuery Plugin: http://flaviusmatis.github.io/simplePagination.js/
-    // Pagination des commentaires (plugin Jquery simplePagination.js)
-    /*
-    var items = $(".list-wrapper");
-    var numItems = items.length;
-    var perPage = 8;
-
-    items.slice(perPage).hide();
-
-    $('#pagination-container').pagination({
-        items: numItems,
-        itemsOnPage: perPage,
-        prevText: "&laquo;",
-        nextText: "&raquo;",
-        onPageClick: function (pageNumber) {
-            var showFrom = perPage * (pageNumber - 1);
-            var showTo = showFrom + perPage;
-            items.hide().slice(showFrom, showTo).show();
-        }
-    });
-    */
-
+    
 });
