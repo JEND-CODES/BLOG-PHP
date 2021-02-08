64bfb08fb55bc1b777c1b6f09740504ff12028ec
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2021-02-09 00:09:16 +0100

    Update One.php

--- views/One.php
+++ views/One.php
@@ -1,8 +1,8 @@
 
-<?php require_once('views/header.php'); ?>
+<?php require_once 'views/header.php'; ?>
 
 <div class="big-spacer"></div>
 
 <h2>ONE</h2>
 
-<?php require_once('views/footer.php'); ?>
+<?php require_once 'views/footer.php'; ?>
