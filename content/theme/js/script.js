efa4c4f11f6cf15e0b34f7c19e5821956a360a47
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2021-02-09 02:08:59 +0100

    Codacy Double Quotes !

--- content/theme/js/script.js
+++ content/theme/js/script.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
 
-    // Copie de valeurs d'un input à un autre dans le process de vérifications des pseudos disponibles (voir viewCheckusername.php)
-    const copy_value = $('#pseudo_checked').val(); // get the value from the first input
+    // Copie de valeurs d"un input à un autre dans le process de vérifications des pseudos disponibles (voir viewCheckusername.php)
+    const copy_value = $("#pseudo_checked").val(); // get the value from the first input
     $("#input_checked").val(copy_value); // set the value to another input
 
     // Scroll to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 50) {
-            $('#back-to-top').fadeIn();
+            $("#back-to-top").fadeIn();
         } else {
-            $('#back-to-top').fadeOut();
+            $("#back-to-top").fadeOut();
         }
     });
     // scroll body to 0px on click
-    $('#back-to-top').click(function () {
-        $('body,html').animate({
+    $("#back-to-top").click(function () {
+        $("body,html").animate({
             scrollTop: 0
         }, 400);
         return false;
